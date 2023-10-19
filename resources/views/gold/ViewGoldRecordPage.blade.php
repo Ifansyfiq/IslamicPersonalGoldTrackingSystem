440611c752ab2a47b2cf60d380e174457bad4e4c
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-10-19 11:51:48 +0800

    update view gold record page

--- resources/views/gold/ViewGoldRecordPage.blade.php
+++ resources/views/gold/ViewGoldRecordPage.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="block mb-8">
+            <div class="block mb-8">
                 <a href="{{ route('gold.index') }}" class="text-black font-bold py-2 px-4 rounded ">Add Task</a>
             </div>
             <div class="flex flex-col">
@@ -16,52 +16,53 @@
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                             <table class="min-w-full divide-y divide-gray-200 w-full">
                                 <thead>
-                                <tr>
-                                    <th scope="col" width="50" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        ID
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Gold Name
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Purity (k)
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Weight (g)
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Status
-                                    </th>
-                                    <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
+                                    <tr>
+                                        <th scope="col" width="50" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            ID
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Gold Name
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Purity (k)
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Weight (g)
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Status
+                                        </th>
+                                        <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
 
-                                    </th>
-                                </tr>
+                                        </th>
+                                    </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200">
                                     <tr>
                                         @forelse ($golds as $gold)
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                        {{($gold->id)}}
+                                            {{($gold->id)}}
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                        {{($gold->gold_name)}}
+                                            {{($gold->gold_name)}}
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                        {{($gold->gold_purity)}}
+                                            {{($gold->gold_purity)}}
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                        {{($gold->weight)}}
+                                            {{($gold->weight)}}
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                        {{($gold->status)}}
+                                            {{($gold->status)}}
                                         </td>
-                                       
+
+
                                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                            <a href="{{ route('gold.index') }}" class="text-blue-600 hover:text-blue-900 mb-2 mr-2">View</a>
+                                            <a href="{{ route('gold.show', [$gold->id]) }}" class="text-blue-600 hover:text-blue-900 mb-2 mr-2">View</a>
                                             <a href="{{ route('gold.index') }}" class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2">Edit</a>
                                             <form class="inline-block" action="{{ route('gold.index') }}" method="POST" onsubmit="return confirm('Are you sure?');">
                                                 <input type="hidden" name="_method" value="DELETE">
@@ -71,8 +72,8 @@
                                         </td>
                                     </tr>
                                     @empty
-                                         <p class="text-warning">No record available</p>
-                                        @endforelse
+                                    <p class="text-warning">No record available</p>
+                                    @endforelse
                                 </tbody>
                             </table>
                         </div>
