4d525950efcbdfcb8dd827419678247010e2e35e
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-11-10 21:47:03 +0800

    update view pawnshop details page with attribute

--- resources/views/pawnshop/ViewPawnshopDetailsPage.blade.php
+++ resources/views/pawnshop/ViewPawnshopDetailsPage.blade.php
@@ -27,29 +27,116 @@
 
                                 <tr>
 
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-semibold text-gray-900" style="display: block;">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-semibold text-gray-900">
                                         {{ $pawnshop->arrahnu_name }}
                                     </td>
+                                    <td></td>
 
-                                    <td class="px-6 py-1 whitespace-nowrap text-sm font-semibold text-gray-900" style="display: block;">
+                                <tr>
+                                    <td class="px-6 py-1 whitespace-nowrap text-sm font-semibold text-gray-900">
                                         Loan Details
                                     </td>
+                                    <td></td>
+                                </tr>
 
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900" style="display: block;">
-
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        Current Margin(%): {{ $pawnshop->margin }}
                                     </td>
 
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900" style="display: block;">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        Loan Duration(month): {{ $pawnshop->loan_duration }}
+                                    </td>
+                                </tr>
 
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        Safekeeping Rate:
+                                    </td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 font-semibold">
+                                        Item Values (RM)
+                                    </td>
+
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 font-semibold">
+                                        Safekeeping rate monthly by RM100/item values
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        First 2,000 & below
                                     </td>
 
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
 
+                                        {{ ($pawnshop->safekeep_rate) }}
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        2,001 - 10,000
                                     </td>
 
-
-
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        {{ ($pawnshop->safekeep_rate) }}
+                                    </td>
                                 </tr>
+
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        Above 10,001
+                                    </td>
+
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                        {{ ($pawnshop->safekeep_rate) }}
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="px-6 py-1 whitespace-nowrap text-sm font-semibold text-gray-900">
+                                        Ar-Rahnu Details
+                                    </td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <td class="px-6 py-1 whitespace-nowrap text-sm text-gray-900">
+                                        Agent name: {{ $pawnshop->agent_name }}
+                                    </td>
+                                    <td class="px-6 py-1 whitespace-nowrap text-sm text-gray-900">
+                                        Operation time: {{ $pawnshop->operation_time }}
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="px-6 py-1 whitespace-nowrap text-sm text-gray-900">
+                                        Agent phone num: {{ $pawnshop->agent_contact_num }}
+                                    </td>
+                                    <td></td>
+                                </tr>
+
+
+                                <tr>
+                                    <td class="px-6 py-1 whitespace-nowrap text-sm text-gray-900">
+                                        Email: {{ $pawnshop->email }}
+                                    </td>
+                                    <td class="px-6 py-1 whitespace-nowrap text-sm text-gray-900">
+                                        Address: {{ $pawnshop->address }}
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td class="px-6 py-1 whitespace-nowrap text-sm text-gray-900">
+                                        Hotline: {{ $pawnshop->hotline }}
+                                    </td>
+                                    <td></td>
+                                </tr>
+
                             </tbody>
                         </table>
                     </div>
