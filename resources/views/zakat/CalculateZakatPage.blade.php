c999ee982318bdc6fc0f08e6c5be3eb613bc3545
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-11-19 12:04:48 +0800

    add first layer of input in calculator zakat page

--- resources/views/zakat/CalculateZakatPage.blade.php
+++ resources/views/zakat/CalculateZakatPage.blade.php
@@ -7,18 +7,56 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="block mb-8">
+            <div class="block mb-8">
                 <h2 class="text-black underline font-semibold ">Gold Zakat Calculation</h2>
             </div>
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">  
-
-                    
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <form>
+                            @csrf
+                            <div>
+                                <label for="year">Year/Haul :</label>
+                                <select id="year" name="year" oninput="urufValue()">
+                                    <option value="2023">2023</option>
+                                    <option value="2022">2022</option>
+                                    <option value="2021">2021</option>
+                                    <option value="2020">2020</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label for="uruf">Uruf Value(g) :</label>
+                                <input type="text" id="uruf" class="form-control" name="uruf" readonly>
+                            </div>
+                            <div>
+                                <label for="goldprice">Gold Price(RM) :</label>
+                                <input type="text" id="goldprice" class="form-control" name="goldprice" readonly>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
 
         </div>
     </div>
+    <script>
+        function urufValue() {
+            var x = document.getElementById("year").value;
+            if (x == "2023") {
+                document.getElementById("uruf").value = "800";
+                document.getElementById("goldprice").value = "254.42";
+            } else if (x == "2022") {
+                document.getElementById("uruf").value = "800";
+                document.getElementById("goldprice").value = "239.55";
+            } else if (x == "2021") {
+                document.getElementById("uruf").value = "800";
+                document.getElementById("goldprice").value = "238.81";
+            } else if (x == "2020") {
+                document.getElementById("uruf").value = "800";
+                document.getElementById("goldprice").value = "164.33";
+            } else
+                document.getElementById("uruf").value = "0";
+        }
+    </script>
+
 </x-app-layout>
