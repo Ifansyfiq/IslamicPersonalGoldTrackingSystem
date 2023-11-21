8272284e65ddffa952b4387b3146a79188ddd70f
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-11-21 16:22:10 +0800

    update addgoldrecordpage

--- resources/views/gold/AddGoldRecordPage.blade.php
+++ resources/views/gold/AddGoldRecordPage.blade.php
@@ -5,64 +5,105 @@
         </h2>
     </x-slot>
 
+
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="block mb-8">
-                <h2 class="text-black underline font-semibold ">Add Record</h2>
+                <h2 class="mb-2 mt-0 text-4xl font-semibold leading-tight text-primary">
+                    Add Records
+                </h2>
             </div>
-            <div class="flex flex-col">
-                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-
-                        <form action="{{route('gold.store')}}" method="POST">
-                            @csrf
-                            <div class="row pt-3">
-                                <div class="control-group col-12">
-                                    <label for="gold_name">Gold Name :</label>
-                                    <input type="text" id="gold_name" class="form-control" name="gold_name" required>
-                                </div>
-                                <div class="control-group col-12 mt-2 pt-3">
-                                    <label for="gold_purity">Purity :</label>
-                                    <input type="text" id="gold_purity" class="form-control" name="gold_purity" required>
-                                </div>
-                                <div class="control-group col-12 mt-2 pt-3">
-                                    <label for="weight">Weight(g) :</label>
-                                    <input type="text" id="weight" class="form-control" name="weight" required>
-                                </div>
-                                <div class="control-group col-12 mt-2 pt-3">
-                                    <label for="buy_shop">Buy Shop :</label>
-                                    <input type="text" id="buy_shop" class="form-control" name="buy_shop" required>
-                                </div>
-                                <div class="control-group col-12 mt-2 pt-3">
-                                    <label for="status">Status :</label>
-                                    <input type="text" id="status" class="form-control" name="status" required>
-                                </div>
-                                <div class="control-group col-12 mt-2 pt-3">
-                                    <label for="buy_price">Buy Price(RM) :</label>
-                                    <input type="text" id="buy_price" class="form-control" name="buy_price" required>
-                                </div>
-                                <div class="control-group col-12 mt-2 pt-3">
-                                    <label for="sell_price">Sell Price(RM) :</label>
-                                    <input type="text" id="sell_price" class="form-control" name="sell_price" required>
-                                </div>
-                                <div class="control-group col-12 mt-2 pt-3">
-                                    <label for="spread">Spread(%) :</label>
-                                    <input type="text" id="spread" class="form-control" name="spread" required>
-                                </div>
-                            </div>
-                            <div class="row mt-2">
-                                <div class="control-group col-12 text-center">
-                                    <button id="btn-submit" class="btn btn-primary">
-                                        Submit
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-
+            <form action="{{route('gold.store')}}" method="POST">
+                @csrf
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="gold_name" id="gold_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+                        <label for="gold_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Gold Name</label>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <select name="gold_purity" id="gold_purity" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+                        <option value="" disabled selected></option>
+                        <option value="24">24</option>
+                        <option value="22">22</option>
+                        <option value="18">18</option>
+                        <option value="14">14</option>
+                        <option value="10">10</option>
+                        </select>
+                        <label for="gold_purity" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Purity</label>
                     </div>
                 </div>
-            </div>
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="weight" id="weight" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+                        <label for="weight" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Weight(g)</label>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <select type="text" name="buy_shop" id="buy_shop" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" onchange="handleGoldPurityChange()" required />
+                        <option value="" disabled selected></option>
+                        <option value="Public Gold Malaysia">Public Gold Malaysia</option>
+                        <option value="Gold Shop">Gold Shop</option>
+                        <option value="Habib Jewels">Habib Jewels</option>
+                        <option value="Poh Kong">Poh Kong</option>
+                        <option value="Poh Kong">Online Gold Shop</option>
+                        </select>
+                        <label for="buy_shop" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Buy Shop</label>
+                    </div>
+                </div>
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <select type="tel" name="status" id="status" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+                        <option value="" disabled selected></option>
+                        <option value="Buy">Buy</option>
+                        <option value="Sell">Sell</option>
+                        <option value="Hold">Hold</option>
+                        <option value="Gift">Gift</option>
+                        <option value="Pawn">Pawn</option>
+                        <option value="Renew Pawn">Renew Pawn</option>
+                        <option value="Other">Other</option>
+                        </select>
+                        <label for="status" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Status</label>
+                    </div>
+                    <div>
+                    </div>
+                </div>
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="tel" name="buy_price" id="buy_price" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+                        <label for="buy_price" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Buy Price(RM)</label>
+                    </div>
+                    <div>
+                    </div>
+                </div>
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="tel" name="sell_price" id="sell_price" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" oninput="spreadVal()" required />
+                        <label for="sell_price" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Sell Price(RM)</label>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="spread" id="spread" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" readonly />
+                        <label for="spread" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Spread(%)</label>
+                    </div>
+                </div>
+                <div class="flex items-center justify-center px-4 py-3 text-center sm:px-6">
+                    <button class="inline-flex items-center px-4 py-2 mr-3 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                        Submit
+                    </button>
+                    <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150" onclick="window.location.href='{{ route('gold.index') }}'">
+                        Cancel
+                    </button>
 
+                </div>
+            </form>
         </div>
     </div>
+    <script>
+        function spreadVal() {
+            var buy_price = document.getElementById("buy_price").value;
+            var sell_price = document.getElementById("sell_price").value;
+            var spread = document.getElementById("spread");
+            var spreadVal = ((sell_price - buy_price) / sell_price) * 100;
+            spread.value = Math.round(spreadVal).toString();
+        }
+    </script>
 </x-app-layout>
