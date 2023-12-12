8d95f5c83a468bacf3b84a710caf9c80faf13a6a
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-12-12 23:44:18 +0800

    delete role id in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('state');
             $table->string('city');
             $table->string('postal_code');
-            // $table->text('role_id');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
