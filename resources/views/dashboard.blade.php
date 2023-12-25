3e544128d5c8590c81bb5156e5d87189d4179231
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-12-25 15:24:01 +0800

    update dashboard layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,33 @@
         </h2>
     </x-slot>
 
+    <link rel="stylesheet" href="{{ asset('css/card.css') }}">
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-               <x-label> Welcome to Islamic Personal Gold Tracking System </x-label>
+
+            <div class="promo_card">
+                <h1>Welcome to myGoldTrace</h1>
+                <span style="font-family: verdana, geneva, sans-serif;">Lorem ipsum dolor sit amet.</span>
+                <button class="custom-btn btn-2" style="display: block;">Learn More</button>
             </div>
+
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
+                <div class="card">
+                    <div class="card-overlay"></div>
+                    <div class="card-inner">Gold Record<br>1<br>HERE</div>
+                </div>
+                <div class="card">
+                    <div class="card-overlay"></div>
+                    <div class="card-inner">Total Gold Weight<br>2<br>HERE</div>
+                </div>
+                <div class="card">
+                    <div class="card-overlay"></div>
+                    <div class="card-inner">YOUR<br>3<br>HERE</div>
+                </div>
+            </div>
+
         </div>
     </div>
+
 </x-app-layout>
