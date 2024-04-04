cfea84c8c5526d1aa283c2b97f58d78a7b17153e
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2024-04-04 15:10:40 +0800

    show image in correct path

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -28,7 +28,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Image:</strong>
-                <img src="/images/{{ $product->image }}" width="500px">
+                <img src="/image/{{ $product->image }}" width="200px">
             </div>
         </div>
     </div>
